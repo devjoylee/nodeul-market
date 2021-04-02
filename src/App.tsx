bbdaf9a5c5f867050f82e9c2a2ecd2bd45bdb0dc
Joy Lee <joylee.dev@gmail.com>
2021-04-02 16:51:36 +0900

    1.마크업 : main 아이템 그리드 생성

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,36 @@ function App() {
         'https://pds.joins.com/news/component/htmlphoto_mmdata/201607/04/htm_2016070413930722912.jpg',
       description: '오늘 갓 수확한 감자입니다!',
     },
+    {
+      id: 2,
+      name: '닭가슴살',
+      category: '육류',
+      price: 1500,
+      amount: 5,
+      image:
+        'https://img-cf.kurly.com/shop/data/goodsview/20210106/gv00000063313_1.jpg',
+      description: '수지스 냉장 닭가슴살 5개 있어여',
+    },
+    {
+      id: 3,
+      name: '두유',
+      category: '유제품',
+      price: 1000,
+      amount: 20,
+      image:
+        'https://shop2.daumcdn.net/thumb/R500x500/?fname=http%3A%2F%2Fshop2.daumcdn.net%2Fshophow%2Fp%2FB5099232161.jpg%3Fut%3D20201027002331',
+      description: '두유 같이 먹어용',
+    },
+    {
+      id: 4,
+      name: '사과',
+      category: '과일',
+      price: 1000,
+      amount: 10,
+      image:
+        'https://img1.tmon.kr/cdn2/deals/2019/02/18/1833260358/1833260358_front_tXcjNIShGQ.jpg',
+      description: '아침 사과 함께 해요~',
+    },
   ];
 
   return (
--- src/components/Body/Item.tsx
+++ src/components/Body/Item.tsx
@@ -7,13 +7,19 @@ interface ItemProps {
 
 export function Item({ item }: ItemProps) {
   return (
-    <div>
-      <h3>{item.name}</h3>
-      <p>카테고리 : {item.category}</p>
-      <p>수량 : {item.amount}</p>
-      <p>{item.price}원</p>
-      <p>{item.description}</p>
-      <img src={item.image} alt={item.name} />
+    <div className="item-wrapper grid-lg grid-md grid-sm">
+      <div className="item">
+        <div className="item__imagebox">
+          <img src={item.image} alt={item.name} />
+        </div>
+        <div className="item__detail">
+          <h3>{item.name}</h3>
+          <p>카테고리 : {item.category}</p>
+          <p>수량 : {item.amount}</p>
+          <p>{item.price}원</p>
+          <p>{item.description}</p>
+        </div>
+      </div>
     </div>
   );
 }
