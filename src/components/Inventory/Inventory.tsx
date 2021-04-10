51c82f1bf3ea1d79a4e4d84f2703234497865de3
Joy Lee <joylee.dev@gmail.com>
2021-04-10 16:31:09 +0900

    main category filter

--- src/components/Inventory/Inventory.tsx
+++ src/components/Inventory/Inventory.tsx
@@ -9,19 +9,75 @@ interface InventoryProps {
 }
 
 export function Inventory({ inventoryItems, addToCart }: InventoryProps) {
+	const filterByCategory = (
+		e: React.MouseEvent<HTMLParagraphElement, MouseEvent>
+	) => {
+		const selected = e.currentTarget.id;
+		console.log(selected);
+	};
+
 	return (
 		<section className="inventory">
 			<Container margin>
+				<div className="inventory__filter">
+					<div className="filter-box">
+						<p
+							className="filter-name"
+							id="all"
+							onClick={(e) => filterByCategory(e)}
+						>
+							🛒 All Items
+						</p>
+					</div>
+					<div className="filter-box">
+						<p
+							className="filter-name"
+							id="fruit"
+							onClick={(e) => filterByCategory(e)}
+						>
+							🍎 Fruit
+						</p>
+					</div>
+					<div className="filter-box">
+						<p
+							className="filter-name"
+							id="dairy"
+							onClick={(e) => filterByCategory(e)}
+						>
+							🧀 Dairy Products
+						</p>
+					</div>
+					<div className="filter-box">
+						<p
+							className="filter-name"
+							id="vegitable"
+							onClick={(e) => filterByCategory(e)}
+						>
+							🥑 Vegitable
+						</p>
+					</div>
+					<div className="filter-box">
+						<p
+							className="filter-name"
+							id="frozen"
+							onClick={(e) => filterByCategory(e)}
+						>
+							🧊 Frozen
+						</p>
+					</div>
+				</div>
 				{inventoryItems.length === 0 ? (
 					// if the inventory is empty
 					<div className="inventory-empty">No Items available</div>
 				) : (
 					// if the inventory has items
-					<div className="item-list">
-						{inventoryItems.map((item) => (
-							<Item key={item.id} item={item} addToCart={addToCart} />
-						))}
-					</div>
+					<>
+						<div className="item-list">
+							{inventoryItems.map((item) => (
+								<Item key={item.id} item={item} addToCart={addToCart} />
+							))}
+						</div>
+					</>
 				)}
 			</Container>
 		</section>
--- src/components/Inventory/Item.tsx
+++ src/components/Inventory/Item.tsx
@@ -8,6 +8,23 @@ interface ItemProps {
 }
 
 export function Item({ item, addToCart }: ItemProps) {
+	const colorizeCategory = () => {
+		switch (item.category) {
+			case "fruit":
+				return "label-color-1";
+			case "dairy":
+				return "label-color-2";
+			case "vegitable":
+				return "label-color-3";
+			case "frozen":
+				return "label-color-4";
+			case "etc":
+				return "label-color-5";
+			default:
+				throw new Error("Unhandled input is selected");
+		}
+	};
+
 	return (
 		<div className="item-wrapper grid-lg grid-md grid-sm">
 			<div className="item">
@@ -16,7 +33,7 @@ export function Item({ item, addToCart }: ItemProps) {
 				</div>
 				<ul className="item__detail">
 					<li className="category">
-						<span>{item.category}</span>
+						<span className={colorizeCategory()}>{item.category}</span>
 					</li>
 					<li className="name">{item.name}</li>
 					<li className="stock">stock : {item.amount}</li>
